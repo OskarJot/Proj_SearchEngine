96b5da8881053f95f99be7e05ccf034a5a5a69a6
OskarJot <oskar.jarnot@gmail.com>
2023-03-06 16:33:35 +0100

    search engine new function

--- js.js
+++ js.js
@@ -1,1 +1,21 @@
 console.log('Oskar');
+
+const inputValue = document.querySelector('.header-search');
+let drinkList = document.querySelectorAll('li');
+let newDrinkList = [];
+let oldDrinkList = [];
+
+const searchValue = (e) => {
+	const text = e.target.value.toLowerCase();
+	console.log(text);
+
+	drinkList.forEach((el) => {
+		if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+			el.style.display = 'block';
+		} else {
+			el.style.display = 'none';
+		}
+	});
+};
+
+inputValue.addEventListener('keyup', searchValue);
